88b6b4f022deab1ceab4481fa220d8f2723a9f18
marius-mauritz@gmx.de <marius-mauritz@gmx.de>
2019-05-04 23:34:35 +0200

    tabelle

--- frontend/lib/src/view/components/calendar_main.dart
+++ frontend/lib/src/view/components/calendar_main.dart
@@ -3,7 +3,6 @@ import 'package:angular_router/angular_router.dart';
 import 'package:demo/src/model/week.dart';
 import 'package:demo/src/view/services/calendar_service.dart';
 
-
 @Component(
   selector: 'calendarMain',
   templateUrl: 'calendar_main.html',
@@ -13,20 +12,49 @@ import 'package:demo/src/view/services/calendar_service.dart';
 class CalendarComponent implements OnInit {
   // service Klasse für ORM
   CalendarComponent(this._calendarService);
-  final CalendarService _calendarService;
 
+  final CalendarService _calendarService;
 
   @override
   void ngOnInit() async {
     // do something when drawn
     // like DB connections
   }
-  static const List<String> week =[
+
+  static const List<String> week = [
     "Montag",
     "Dienstag",
     "Mittwoch",
     "Donnerstag",
     "Freitag",
     "Samstag",
-    "Sonntag"];
+    "Sonntag"
+  ];
+
+  static var month = DateTime.now().month;
+  static var year = DateTime.now().year;
+  static int currentMonthLength = DateTime.utc(year, month + 1, 0).day;
+  static int prevMonthLength = DateTime.utc(year, month - 1, 0).day;
+  static int firstWeekDayOfMonth = DateTime.utc(year, month, 1).weekday;
+  static int weekOffSet = prevMonthLength - firstWeekDayOfMonth;
+
+  // creates iterable startin from Mo till Su
+  static Iterable weekOfMonth(int n) sync* {
+    int k = n*7 - firstWeekDayOfMonth +1;
+    int count = 0;
+    if (n == 0) {
+      k = weekOffSet;
+    }
+    while (count < 7) {
+      count++;
+      k++;
+      if (k >= prevMonthLength && n == 0) {
+        k = 1;
+      }
+      if (k >= currentMonthLength && n!=0){
+        k=1;
+      }
+      yield k;
+    }
+  }
 }
