f4e8c362b05ba816f6ea5f7257bddc4f28abc405
Lennart <lennart.siedschlag@stud.th-luebeck.de>
2019-05-07 14:11:25 +0200

    Routing versuch nr.3

--- frontend/lib/src/view/services/appointment_service.dart
+++ frontend/lib/src/view/services/appointment_service.dart
@@ -23,9 +23,9 @@ class AppointmentService {
   Future<Appointment> update(Appointment appointment) async {
    /* for (var mockAppointment in mockAppointments) {
       if (mockAppointment.id == appointment.id) mockAppointment = appointment;
-        return appointment;
+        return mockAppointment;
     }*/
-
+    /*
     try {
       final url = '$_appointmentUrl/${appointment.id}';
       final response =
@@ -33,20 +33,20 @@ class AppointmentService {
       return Appointment.fromJson(_extractData(response) as Map<int, String>);
     } catch (e) {
       throw _handleError(e);
-    }
+    }*/
   }
 
   Future<Appointment> get(int id) async {
-    /*for (var appointment in mockAppointments) {
+    for (var appointment in mockAppointments) {
       if (appointment.id == id) return appointment;
-    }*/
+    }
 
-    try {
+    /*try {
       final response = await _http.get('$_appointmentUrl/$id');
       return Appointment.fromJson(_extractData(response) as Map<int, String>);
     } catch (e) {
       throw _handleError(e);
-    }
+    }*/
   }
 
   Future<Appointment> create(String name) async {
