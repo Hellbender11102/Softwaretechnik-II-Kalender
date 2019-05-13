672f5e64af47a8f54145de2e8754f249cdf29460
marius-mauritz@gmx.de <marius-mauritz@gmx.de>
2019-05-13 12:37:17 +0200

    hübsch gemacht mit sinnvollen funktionen

--- frontend/lib/src/model/person.dart
+++ frontend/lib/src/model/person.dart
@@ -1,19 +1,47 @@
-abstract class Person{
+abstract class Person {
+  Person(this.nickname, this.surname, this.name, this.email);
+
   String nickname;
   String surname;
   String name;
   String email;
-
-
+  String mobileNo;
 }
 
 class User extends Person {
-  List<Contact> contactlist;
+  // ggf persistente Daten von einer Service Routine
+  List<Contact> _contactlist = List();
 
+  User(String nickname, String surname, String name, String email)
+      : super(nickname, surname, name, email);
 
+  // Adds a single contact, no duplicate by contactcode
+  void addContact(Contact con) {
+    if (_contactlist.any((ele) => ele.contactCode != con.contactCode)) {
+      _contactlist.add(con);
+    }
+  }
+
+  // adds all Contacts, no duplicates by contactcode
+  void addContacts(List<Contact> list) => list.forEach(addContact);
+
+  // removes contact from list
+  void rmvContact(Contact con) => _contactlist.remove(con);
+
+  // removes multiple Contacts
+  void rmvAllContacts(List<Contact> list) => list.forEach(rmvContact);
 }
-class Contact extends Person{
+
+class Contact extends Person {
+  Contact(String nickname, String surname, String name, String email,
+      this.contactCode, this.note)
+      : super(nickname, surname, name, email);
   String contactCode;
   String note;
-
 }
+
+List<Contact> list = []
+  ..add(Contact("dude", "owski", "mike", "Mike.owski@gmail.com", "PQ459",
+      "Pretty average Guy."))
+  ..add(Contact("dude", "owski", "mike", "Mike.owski@gmail.com", "PQ458",
+      "the underwhelming Guy."));
