81d33f6a4367f056ea93a0ca6ef65e2100a3b274
Mauritz, Falk Marius <marius.mauritz@stud.th-luebeck.de>
2019-05-25 10:27:33 +0200

    Docker

--- middleman/migrations/00000001_initial.migration.dart
+++ middleman/migrations/00000001_initial.migration.dart
@@ -45,7 +45,63 @@ class Migration1 extends Migration {
   }
 
   @override
-  Future downgrade() async {}
+  Future downgrade() async {
+    final _appointments = [
+      {
+        "id": 1,
+        "name": "eins",
+        "date": "2019-05-07",
+        "time": "12:00",
+        "duration": "04:00",
+        "location": "Technischehochschule Lübeck"
+      },
+      {
+        "id": 2,
+        "name": "zwei",
+        "date": "2019-05-07",
+        "time": "12:00",
+        "duration": "04:00",
+        "location": "Technischehochschule Lübeck"
+      },
+      {
+        "id": 3,
+        "name": "drei",
+        "date": "2019-05-07",
+        "time": "12:00",
+        "duration": "04:00",
+        "location": "Technischehochschule Lübeck"
+      },
+      {
+        "id": 4,
+        "name": "vier",
+        "date": "2019-05-07",
+        "time": "12:00",
+        "duration": "04:00",
+        "location": "Technischehochschule Lübeck"
+      },
+      {
+        "id": 5,
+        "name": "fünf",
+        "date": "2019-05-07",
+        "time": "12:00",
+        "duration": "04:00",
+        "location": "Technischehochschule Lübeck"
+      },
+    ];
+
+    _appointments.forEach((Map<String, dynamic> map) async => await database
+        .store
+        .execute("INSERT INTO _Appointment (name,time,date,duration,location) VALUES (@name,@time,@date,@duration,@location)",
+        substitutionValues: {
+          "name": map['name'],
+          "time": map['time'],
+          "date": map['date'],
+          "duration": map['duration'],
+          "location": map['location'],
+        }));
+
+
+  }
 
   @override
   Future seed() async {
