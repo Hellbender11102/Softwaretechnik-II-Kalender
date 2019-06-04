7947ab8dbe640e1deeba44e730251d3a27420e7e
HellLab <HellLab@DESKTOP-PA26FQM>
2019-06-04 12:43:35 +0200

    example tests

--- middleman/test/example_test.dart
+++ middleman/test/example_test.dart
@@ -1,10 +1,20 @@
 import 'harness/app.dart';
 
-Future main() async {
-  final harness = Harness()..install();
+ main() async {
+ // final harness = Harness()..install();
 /*
   test("GET /heroes returns 200 {'key': 'value'}", () async {
     expectResponse(await harness.agent.get("/heroes"), 200, body: {"key": "value"});
   });
   */
+
+  test("String.split() splits the string on the delimiter", () {
+    var string = "foo,bar,baz";
+    expect(string.split(","), equals(["foo", "bar", "baz"]));
+  });
+
+  test("String.trim() removes surrounding whitespace", () {
+    var string = "  foo ";
+    expect(string.trim(), equals("foo"));
+  });
 }
