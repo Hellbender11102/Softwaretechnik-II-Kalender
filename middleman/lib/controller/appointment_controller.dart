2c509b1aac7ea359e33e334a3beaeb9485ab391a
Lennart <lennart.siedschlag@stud.th-luebeck.de>
2019-06-05 12:47:58 +0200

    neue seed

--- middleman/lib/controller/appointment_controller.dart
+++ middleman/lib/controller/appointment_controller.dart
@@ -56,11 +56,12 @@ class AppointmentController extends ResourceController {
   @Operation.post()
   Future<Response> newAppointment() async {
     final Map<String, dynamic> body = await request.body.decode();
-    final query = Query<Appointment>(context)..values.read(body);
+    final query = Query<Appointment>(context)..values.read(body, ignore: ["id"]);
     final insertedHero = await query.insert();
 
     return Response.ok(insertedHero);
   }
+
   @Operation.put()
   Future<Response> updateAppointment() async {
     final Map<String, dynamic> body = await request.body.decode();
--- middleman/migrations/00000001_initial.migration.dart
+++ middleman/migrations/00000001_initial.migration.dart
@@ -193,6 +193,58 @@ class Migration1 extends Migration {
 
   @override
   Future seed() async {
+    final _appointments = [
+      {
+        "id": 1,
+        "name": "eins",
+        "year": 2019,
+        "month": 7,
+        "day": 22,
+        "time": "12:00",
+        "duration": "04:00",
+        "location": "Technischehochschule Lübeck"
+      },
+      {
+        "id": 2,
+        "name": "zwei",
+        "year": 2019,
+        "month": 5,
+        "day": 22,
+        "time": "12:00",
+        "duration": "04:00",
+        "location": "Technischehochschule Lübeck"
+      },
+      {
+        "id": 3,
+        "name": "drei",
+        "year": 2019,
+        "month": 5,
+        "day": 24,
+        "time": "12:00",
+        "duration": "04:00",
+        "location": "Technischehochschule Lübeck"
+      },
+      {
+        "id": 4,
+        "name": "vier",
+        "year": 2019,
+        "month": 5,
+        "day": 21,
+        "time": "12:00",
+        "duration": "04:00",
+        "location": "Technischehochschule Lübeck"
+      },
+      {
+        "id": 5,
+        "name": "fünf",
+        "year": 2019,
+        "month": 7,
+        "day": 22,
+        "time": "12:00",
+        "duration": "04:00",
+        "location": "Technischehochschule Lübeck"
+      },
+    ];
 
     final _contacts = [
       {
@@ -201,9 +253,41 @@ class Migration1 extends Migration {
         "name": "zwei",
         "email": "hallo@hallo.gmx.de",
         "contactCode": "abcd",
-        "note": "12:00",
+        "note": "12:00"
+      },
+      {
+        "nickname": "mate",
+        "surname": "funf",
+        "name": "sechs",
+        "email": "hello@world.gmx.de",
+        "contactCode": "efgh",
+        "note": "test"
+      }
+      ];
+
+    final _users = [
+      {
+        "nickname": "dude2",
+        "surname": "drei",
+        "name": "vier",
+        "email": "hello@hello.gmx.de",
+        "contactCode": "12345",
+        "password": "Calendar123",
+        "mobileNo": ""
       }];
 
+    _appointments.forEach((Map<String, dynamic> map) async =>
+    await database.store.execute(
+        "INSERT INTO _Appointment (name,time,year,month,day,duration,location) VALUES (@name,@time,@year,@month,@day,@duration,@location)",
+        substitutionValues: {
+          "name": map['name'],
+          "time": map['time'],
+          "year": map['year'],
+          "month": map['month'],
+          "day": map['day'],
+          "duration": map['duration'],
+          "location": map['location'],
+        }));
 
     _contacts.forEach((Map<String, String> map) async =>
     await database.store.execute(
@@ -215,6 +299,19 @@ class Migration1 extends Migration {
           "email": map['email'],
           "contactCode": map['contactCode'],
           "note": map['note']
+        }));
+
+    _users.forEach((Map<String, String> map) async =>
+    await database.store.execute(
+        "INSERT INTO _User (nickname,surname,name,email,contactCode,password,mobileNo) VALUES (@nickname,@surname,@name,@email,@contactCode,@password,@mobileNo)",
+        substitutionValues: {
+          "nickname": map['nickname'],
+          "surname": map['surname'],
+          "name": map['name'],
+          "email": map['email'],
+          "contactCode": map['contactCode'],
+          "password": map['password'],
+          "mobileNo": map['mobileNo']
         }));
 
   }
