8ff83e52ce69e805a9a104848d4ddc083d6ebc8b
Marius Mauritz <marius.mauritz@stud.th-luebeck.de>
2019-06-05 14:28:31 +0200

    sachem

--- middleman/lib/model/person.dart
+++ middleman/lib/model/person.dart
@@ -1,19 +1,19 @@
 import 'package:middleman/middleman.dart';
 
 class Person extends ManagedObject<_Person> implements _Person {}
+
 class User extends ManagedObject<_User> implements _User {}
+
 class Contact extends ManagedObject<_Contact> implements _Contact {}
 
-
 abstract class _Person {
-
   @primaryKey
   int id;
 
-  @Column(unique: false)
+  @Column(unique: true, nullable: true)
   String contactCode;
 
-  @Column(unique: false)
+  @Column(unique: true)
   String nickname;
 
   @Column(unique: false)
@@ -24,25 +24,17 @@ abstract class _Person {
 
   @Column(unique: true)
   String email;
-
-
-
 }
 
-class _Contact extends _Person{
-
-  @Column(unique: true)
+class _Contact extends _Person {
+  @Column(unique: false,nullable: true)
   String note;
-
 }
 
 class _User extends _Person {
-
   @Column(unique: false)
   String password;
 
-  @Column(unique: true)
+  @Column(unique: false, nullable: true)
   String mobileNo;
-
 }
-
